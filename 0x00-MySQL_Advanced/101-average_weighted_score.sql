16952f22f5ef4037e0149a7ae0f999d621a5db59
samuelselasi <samelsekasi@gmail.com>
2023-10-12 01:21:58 +0000

    Add SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,15 @@
+-- An SQL script that creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students
+DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+	BEGIN
+		UPDATE users,
+			(SELECT users.id, SUM(score * weight) / SUM(weight) AS wei_avg
+				FROM users
+				JOIN corrections ON users.id = corrections.user_id
+				JOIN projects ON projects.id = corrections.project_id
+				GROUP BY users.id)
+			AS weighted_avg
+		SET users.average_score = weighted_avg.wei_avg
+		WHERE users.id - weighted_avg.id;
+	END $$
