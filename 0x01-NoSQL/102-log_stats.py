2446138b305cee01bdd0525acaea7aedec6632e6
samuelselasi <samelsekasi@gmail.com>
2023-10-17 09:54:59 -0700

    Fix

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -2,22 +2,39 @@
 """Module containing script that returns Nginx stats stored in MongoDB"""
 import pymongo
 
-client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
-logs = client.logs.nginx
-print("{} logs".format(logs.estimated_document_count()))
 
-print("Methods:")
-for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-    count = logs.count_documents({'method': method})
-    print("\tmethod {}: {}".format(method, count))
+def collection(db: dict) -> int:
+    """Function to retroeve logs information"""
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    return logs.count_documents(db)
 
-status = logs.count_documents({'method': 'GET', 'path': "/status"})
-print("{} status check".format(status))
-print("IPs:")
 
-topIps = logs.aggregate(
-        [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-            {"$sort": {"count": -1}}, {"$limit": 10},
-            {"$project": {"_id": 0, "ip": "$_id", "count": 1}}])
-for ip in topIps:
-    print("\t{}: {}".format(ip.get('ip'), ip.get('count')))
+def main():
+    """Function that returns stats about Nginx logs stored in MongoDB"""
+
+    print(f"{collection({})} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {collection({'method': 'GET'})}")
+    print(f"\tmethod POST: {collection({'method': 'POST'})}")
+    print(f"\tmethod PUT: {collection({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {collection({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {collection({'method': 'DELETE'})}")
+    print(f"{collection({'method': 'GET', 'path': '/status'})} status check")
+
+    print("IPs:")
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    ips = logs.aggregate(
+            [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+                {"$sort": {"count": -1}}])
+    count = 0
+    for ip in ips:
+        if count == 10:
+            break
+        print(f"\t{ip.get('_id')}: {ip.get('count')}")
+        count += 1
+
+
+if __name__ == "__main__":
+    main()
