a0ccb4e944b7cae36e2153844d6ee11ad90fd2a3
samuelselasi <samelsekasi@gmail.com>
2023-10-17 04:00:55 -0700

    Modify script

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,7 +2,7 @@
 """Script that returns Nginx logs stats stored in MongoDB"""
 import pymongo
 
-
+"""
 client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
 database = client['logs']
 collection = database['nginx']
@@ -21,3 +21,28 @@ status = collection.count_documents({"method": "GET", "path": "/status"})
 print(f"{status} status check")
 
 client.close()
+"""
+
+
+def collection(db: dict) -> int:
+    """Function to retroeve logs information"""
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    logs = client.logs.nginx
+    return logs.count_documents(db)
+
+
+def main():
+    """Function that returns stats about Nginx logs stored in MongoDB"""
+
+    print(f"{collection({})} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {collection({'method': 'GET'})}")
+    print(f"\tmethod POST: {collection({'method': 'POST'})}")
+    print(f"\tmethod PUT: {collection({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {collection({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {collection({'method': 'DELETE'})}")
+    print(f"{collection({'method': 'GET', 'path': '/status'})} status check")
+
+
+if __name__ == "__main__":
+    main()
