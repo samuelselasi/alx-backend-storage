1cb9e83bfba3c74848c546b75a1a237232a290b9
samuelselasi <samelsekasi@gmail.com>
2023-10-18 15:57:24 +0000

    Add function that uses the requests module to obtain the HTML content of a particular URL and returns it

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Module containing function to return HTML content of a particular url"""
+import redis
+import requests
+from functools import wraps
+
+# Create a Redis client instance
+data = redis.Redis()
+
+
+def url_count(method):
+    """Function that returns the number of times a URL was accessed with key"""
+
+    def wrapper(url):
+        """Function that defines the wrapper"""
+
+        key = "cached:" + url
+        value = data.get(key)
+        if value:
+            return value.decode("utf-8")
+
+        count = "count:" + url
+        content = method(url)
+
+        data.incr(count)
+        data.set(key, content, ex=10)
+        return content
+    return wrapper
+
+
+@url_count  # Decorate get_page with url_count function
+def get_page(url: str) -> str:
+    """Function that returns HTML content of a URL"""
+
+    content = requests.get(url)
+    return content.text
+
+
+if __name__ == "__main":
+    get_page('http://slowwly.robertomurray.co.uk')
