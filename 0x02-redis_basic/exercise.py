58d0aeebb7cb6e2d6b13071ae279735871d62fed
samuelselasi <samelsekasi@gmail.com>
2023-10-18 11:47:33 +0000

    Add get methods to the Cache class that returns data in the desired format

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 import redis
 from uuid import uuid4
 from typing import Union
+from typing import Optional, Callable
 
 
 class Cache:
@@ -20,3 +21,30 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    # Task 1: Reading from Redis and recovering original type
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[
+            str, bytes, int, float]:
+        """Method that converts data back to original format"""
+
+        data = self._redis.get(key)
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """Method that automatically paramatize class Cache.get to str"""
+
+        data = self._redis.get(key)
+        return data.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """Method that automatically paramatize class Cache.get to str"""
+
+        data = self._redis.get(key)
+        try:
+            data = int(value.decode("utf-8"))
+        except Exception:
+            data = 0
+        return data
