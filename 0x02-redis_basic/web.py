d1b322c364432fa0dec5e6416bb43f3ad5fa7872
samuelselasi <samelsekasi@gmail.com>
2023-10-18 16:30:11 +0000

    Fix 4 with wrappers

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,10 +2,51 @@
 """Module containing function to return HTML content of a particular url"""
 import redis
 import requests
+from functools import wraps
+from typing import Callable
 
 data = redis.Redis()
 
 
+def call_history(method: Callable) -> Callable:
+    """Function to store the history of inputs and outputs of a function"""
+
+    key = method.__qualname__
+    inputs = key + ":inputs"
+    outputs = key + ":outputs"
+
+    @wraps(method)
+    def wrapper(url: str):
+        """Function that defines wrapper"""
+
+        data.rpush(inputs, url)
+        cached_content = data.get(f"cached:{url}")
+        if cached_content:
+            data.rpush(outputs, cached_content.decode('utf-8'))
+            return cached_content.decode('utf-8')
+
+        content = method(url)
+        data.rpush(outputs, content)
+        return content
+    return wrapper
+
+
+def count_calls(method: Callable) -> Callable:
+    """Function to count the number of times a function is called"""
+
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(url: str):
+        """Function that defines wrapper"""
+
+        data.incr(key)
+        return method(url)
+    return wrapper
+
+
+@call_history
+@count_calls
 def get_page(url: str) -> str:
     """Function that returns HTML content of a URL"""
 
